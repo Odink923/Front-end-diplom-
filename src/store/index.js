7dec9bd4f76894e6c9f22a538168c38833f1f34a
Odink923 <odink923@gmail.com>
2024-01-02 20:07:31 +0200

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import {applyMiddleware, combineReducers, createStore} from "redux";
+const rootReducer = combineReducers({
+
+})
+
+export const store = createStore(rootReducer, applyMiddleware());
+export default rootReducer;
