b29dbf0fca92b083a90889c9270e35417ed0c18c
Kibit Viktoria <vikakib2005@gmail.com>
2024-01-16 13:53:31 +0200

    feat: add SpecificProtez version1 #3

--- src/components/mainpage/SpecificProtez/SpecificProtez.jsx
+++ src/components/mainpage/SpecificProtez/SpecificProtez.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import classes from "../Logo/Logo.module.css";
+import classes from './SpecificProtez.module.css';
 
 const SpecificProtez = () => {
     return (
         <div className={classes.box}>
+            <div className={classes.image}/>
+            <div className={classes.label}>
+                <div className={classes.textWrapper}>
+                    ОСОБЛИВОСТІ <br/>
+                    ПРОТЕЗА
+                </div>
+            </div>
+            <div className={classes.label2}>
+                <div className={classes.textWrapper2}>
+                    Короткий опис того, які послуги пропонуються і всяке таке подібне.
+                    Короткий опис того, які послуги пропонуються і всяке таке подібне
+                    Короткий опис того, які послуги пропонуються і всяке таке подібне.
+                    Короткий опис того, які послуги пропонуються і всяке таке подібне
+                </div>
+            </div>
+            <div className={classes.label3}>
+                <div className={classes.textWrapper3}>
+                    Короткий опис того, які послуги пропонуються і всяке таке подібне.
+                    Короткий опис того, які послуги пропонуються і всяке таке подібне
+                </div>
+            </div>
+            <div className={classes.other}>
 
+            </div>
+            <div className={classes.scroll}>
+
+            </div>
         </div>
     );
 };
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Logo from "../components/mainpage/Logo/Logo";
+import SpecificProtez from "../components/mainpage/SpecificProtez/SpecificProtez";
 
 const MainPage = () => {
     return (
         <div>
             <Logo></Logo>
+            <SpecificProtez></SpecificProtez>
         </div>
     );
 };
