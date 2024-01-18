0ed5742d4ce682a1e009cc662850476554ba1ecd
Odink923 <odink923@gmail.com>
2024-01-18 05:58:53 +0200

    added component Material
    it`s only layout

--- src/components/protezpage/Material/Material.jsx
+++ src/components/protezpage/Material/Material.jsx
@@ -17,6 +17,14 @@ const Material = () => {
                         <div>ІНДИВІДУАЛЬНІСТЬ</div>
                         <div>ОБСЛУГОВУВАННЯ</div>
                     </div>
+                    <div>
+                    <svg width="52" height="25" viewBox="0 0 52 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0.149414 12.25L49.2988 12.25M49.2988 12.25L39.5488 0.999975M49.2988 12.25L39.5488 23.2986" stroke="#2D3361" stroke-width="3"/>
+                    </svg>
+                    </div>
+                </div>
+                <div className={classes.model}>
+
                 </div>
             </div>
         </div>
