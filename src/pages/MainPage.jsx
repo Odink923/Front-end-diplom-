e713fcba7e13344d831a8b168f12feb7cd7acb98
Kibit Viktoria <vikakib2005@gmail.com>
2024-01-26 16:01:35 +0200

    feat: add animation Logo #17

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import classes from './MainPage.module.css'
 import Logo from "../components/mainpage/Logo/Logo";
 import SpecificProtez from "../components/mainpage/SpecificProtez/SpecificProtez";
-import ViewMoreButton from "../components/UI/ViewMoreButton/ViewMoreButton";
-import BionicHandle from "../components/page2/BionicHandle/BionicHandle";
+import BionicProtez from "../components/mainpage/BionicProtez/BionicProtez";
+import AboutCenterProtez from "../components/mainpage/AboutCenterProtez/AboutCenterProtez";
+import AboutUs from "../components/mainpage/aboutUs/AboutUs";
+import PartnerSponsors from "../components/mainpage/PartnerSponsors/PartnerSponsors";
+import WriteToUs from "../components/mainpage/WriteToUs/WriteToUs";
+import Footerr from "../components/UI/Footer/Footerr";
 
 const MainPage = () => {
     return (
         <div className={classes.page}>
-            {/*<Logo></Logo>*/}
-            {/*<SpecificProtez></SpecificProtez>*/}
-            <BionicHandle></BionicHandle>
+            <Logo></Logo>
+            <SpecificProtez></SpecificProtez>
+            <BionicProtez></BionicProtez>
+            <AboutCenterProtez></AboutCenterProtez>
+            <AboutUs></AboutUs>
+            <PartnerSponsors></PartnerSponsors>
+            <WriteToUs></WriteToUs>
+            <Footerr></Footerr>
         </div>
     );
 };
