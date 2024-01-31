bb6d2f94fa3351b531fab3de349134ef046d16dd
Kibit Viktoria <vikakib2005@gmail.com>
2024-01-31 12:45:24 +0200

    feat: add BeatOfUs #27

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ServicesTitle from "../components/servicespage/ServicesTitle/ServicesTitle";
+import BestOfUs from "../components/servicespage/BestOfUs/BestOfUs";
 
 const ServicesPage = () => {
     return (
         <div>
             <ServicesTitle></ServicesTitle>
+            <BestOfUs></BestOfUs>
         </div>
     );
 };
