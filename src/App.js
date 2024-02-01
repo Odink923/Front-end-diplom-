d5c5e3bfd909bc49590f4a4acb1918f4a0872fcf
Kibit Viktoria <vikakib2005@gmail.com>
2024-02-01 12:29:22 +0200

    fix: update TeamOrService #23

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             <Cursor></Cursor>
             <BrowserRouter>
                 <Routes>
-                    <Route path={"/"} element={<MainPage/>}/>
+                    <Route path={"/"} element={<TeamPage/>}/>
                     <Route path={"/protezPage"} element={<ProtezPage/>}/>
                     <Route path={"/teamPage"} element={<TeamPage/>}/>
                     <Route path={"/aboutUsPage"} element={<AboutUsPage/>}/>
--- src/components/comandpage/TeamTitle/TeamTitle.jsx
+++ src/components/comandpage/TeamTitle/TeamTitle.jsx
@@ -8,8 +8,9 @@ const TeamTitle = () => {
         <div className={classes.box}>
             <div className={classes.rectangle}>
 
-                    <Navbar></Navbar>
+                <Navbar></Navbar>
                 <div className={classes.path}>
+                    <div className={classes.pathFrame}>
                     {/*Path component*/}
                     <div className={classes.frame}>
                         <div className={classes.textWrapper}>Головна</div>
@@ -26,17 +27,19 @@ const TeamTitle = () => {
                                 d="M-1.86696e-07 5.53895L19 5.53893M19 5.53893L15.2309 1.18994M19 5.53893L15.2309 9.81006"
                                 stroke="#8C96C1" stroke-width="1.15973"/>
                         </svg>
-                        <div className={classes.div}>Комада</div>
+                        <div className={classes.div}>Команда</div>
                     </div>
                     {/* End component*/}
+                    </div>
                 </div>
+                <div className={classes.container}>
                 <div className={classes.teamOrService}>
                     <TeamOrService text1={"НАША КОМАНДА"} text2={"ПОЗНАЙОМИМОСЬ?"}
-                                   discription={"Короткий опис того, які" +
-                                       "послуги пропонуються і всяке таке подібне." +
+                                   discription={"Короткий опис того, які " +
+                                       "послуги пропонуються і всяке таке подібне. " +
                                        "Короткий опис того, які послуги пропонуються і всяке таке подібне"}></TeamOrService>
                 </div>
-
+                </div>
             </div>
         </div>
     );
