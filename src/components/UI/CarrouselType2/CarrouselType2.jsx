a452be49316ecd9f833ea2ebe19248146098a09f
Odink923 <odink923@gmail.com>
2024-02-10 00:43:46 +0200

    decomposite CarrouselType2

--- src/components/UI/CarrouselType2/CarrouselType2.jsx
+++ src/components/UI/CarrouselType2/CarrouselType2.jsx
@@ -3,10 +3,11 @@ import {useSpringCarousel} from "react-spring-carousel";
 import CaruselItemType2 from "./UI/CaruselItemType2";
 import classes from "./CarrouselType2.module.css"
 import CustomButton from "../Button/CustomButton";
+import SliderButtons from "./UI/SliderButtons/SliderButtons";
 
 
 const CarrouselType2 = ({mockedItems}) => {
-    const [page,setPage] = useState(1);
+
     const {
         carouselFragment,
         slideToPrevItem,
@@ -20,29 +21,10 @@ const CarrouselType2 = ({mockedItems}) => {
             id: i.id,
             renderItem: (
                 <CaruselItemType2 title={i.title} description={i.description} descriptionSecond={i.descriptionSecond}>
-
                 </CaruselItemType2>
             ),
         })),
     });
-    const right = () =>{
-        page>3 ?
-            setPage(1)
-            :
-        setPage(page+1)
-        slideToNextItem();
-    }
-    const left = () =>{
-        page<2 ?
-        setPage(4)
-            :
-        setPage(page-1)
-        slideToPrevItem();
-    }
-    const clickSlide = (i) =>{
-        setPage(i);
-        slideToItem(i-1);
-    }
     return (
         <div>
             <div className={classes.container}>
@@ -52,29 +34,12 @@ const CarrouselType2 = ({mockedItems}) => {
                         {carouselFragment}
                     </div>
                     <div style={{marginTop: "261px"}}>
-                        <CustomButton onClick={left}>
-                            <svg width="52" height="25" viewBox="0 0 52 25" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M51.1484 12.2503L1.99902 12.2502M1.99902 12.2502L11.749 1.00022M1.99902 12.2502L11.749 23.2988"
-                                    stroke="#2D3361" stroke-width="3"/>
-                            </svg>
-                        </CustomButton>
-                        {mockedItems.map(i =>
-                                <span key={i.id}
-                                      onClick={() => clickSlide(i.id)}
-                                      className={page === i.id ? `${classes.activeItem}` : `${classes.notActiveItem}`}>
-                                       {page === i.id ? `(${i.id})` : i.id}
-                                </span>
-                        )}
-                        <CustomButton onClick={right}>
-                            <svg width="52" height="25" viewBox="0 0 52 25" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M-4.82948e-07 12.2503L49.1494 12.2502M49.1494 12.2502L39.3994 1.00022M49.1494 12.2502L39.3994 23.2988"
-                                    stroke="#2D3361" stroke-width="3"/>
-                            </svg>
-                        </CustomButton></div>
+                      <SliderButtons slideToPrevItem={slideToPrevItem}
+                                     slideToNextItem={slideToNextItem}
+                                     slideToItem={slideToItem}
+                                     mockedItems={mockedItems}
+                      ></SliderButtons>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/components/UI/CarrouselType2/UI/SliderButtons/SliderButtons.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import CustomButton from "../../../Button/CustomButton";
+import classes from "../../CarrouselType2.module.css";
+
+const SliderButtons = ({slideToNextItem,slideToPrevItem,slideToItem,mockedItems}) => {
+    const [page,setPage] = useState(1);
+    const right = () =>{
+        page>3 ?
+            setPage(1)
+            :
+            setPage(page+1)
+        slideToNextItem();
+    }
+    const left = () =>{
+        page<2 ?
+            setPage(4)
+            :
+            setPage(page-1)
+        slideToPrevItem();
+    }
+    const clickSlide = (i) =>{
+        setPage(i);
+        slideToItem(i-1);
+    }
+    return (
+        <div>
+            <CustomButton onClick={left}>
+                <svg width="52" height="25" viewBox="0 0 52 25" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M51.1484 12.2503L1.99902 12.2502M1.99902 12.2502L11.749 1.00022M1.99902 12.2502L11.749 23.2988"
+                        stroke="#2D3361" stroke-width="3"/>
+                </svg>
+            </CustomButton>
+            {mockedItems.map(i =>
+                <span key={i.id}
+                      onClick={() => clickSlide(i.id)}
+                      className={page === i.id ? `${classes.activeItem}` : `${classes.notActiveItem}`}>
+                                       {page === i.id ? `(${i.id})` : i.id}
+                                </span>
+            )}
+            <CustomButton onClick={right}>
+                <svg width="52" height="25" viewBox="0 0 52 25" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M-4.82948e-07 12.2503L49.1494 12.2502M49.1494 12.2502L39.3994 1.00022M49.1494 12.2502L39.3994 23.2988"
+                        stroke="#2D3361" stroke-width="3"/>
+                </svg>
+            </CustomButton>
+        </div>
+    );
+};
+
+export default SliderButtons;
